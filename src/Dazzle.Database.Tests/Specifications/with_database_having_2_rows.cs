07e79e830d8dd14b2b5359b584b58bea153a802f
Daniel Portella <dmportell@hotmail.com>
2012-10-08 00:53:53 +0100

    Few enhancements to the source including small shuffle
    added idisposable to dazzle database client
    modified idisposable on leveldbstorage
    added idisposable to specification context
    modified the way specification context and child classes are used
    added irony dll's to the repo
    modified idisposable for in memory storage
    added idisposable to dazzle server
    modified enumerator handling on Select Operation
    added dummy license (needs to be done)
    added aggregated open source licenses

--- src/Dazzle.Database.Tests/Specifications/with_database_having_2_rows.cs
+++ src/Dazzle.Database.Tests/Specifications/with_database_having_2_rows.cs
@@ -4,14 +4,8 @@ namespace Dazzle.Database.Tests
 {
     public abstract class with_database_having_2_rows : SpecificationContext
     {
-        protected IStorage storage;
-        protected DazzleDatabase db;
-
         public override void Given()
         {
-            this.storage = new InMemoryStorage();
-            this.db = new DazzleDatabase(this.storage);
-
             this.db.ExecuteQuery("update users set firstname = 'jane', lastname = 'tompson' where key = 1"); 
             this.db.ExecuteQuery("update users set firstname = 'bob', lastname = 'smith' where key = 2");
         }
